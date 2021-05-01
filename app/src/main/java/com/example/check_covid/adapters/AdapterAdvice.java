1b6fa40c2a2c04c471ade0820ae5a4cb7473fc2a
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-01 15:44:30 +0200

    create adapters

---
+++ app/src/main/java/com/example/check_covid/adapters/AdapterAdvice.java
@@ -0,0 +1,27 @@
+package com.example.check_covid.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AdapterAdvice extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
