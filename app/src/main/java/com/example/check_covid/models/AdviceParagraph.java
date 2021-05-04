c51afe5083af49a603e206106780cf37ce06b82f
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-04 13:50:34 +0200

    SET AdviceParagraph CLASS

--- app/src/main/java/com/example/check_covid/models/AdviceParagraph.java
+++ app/src/main/java/com/example/check_covid/models/AdviceParagraph.java
@@ -1,4 +1,39 @@
 package com.example.check_covid.models;
 
 public class AdviceParagraph {
+    String Title;
+    String Parag;
+    String AfterWhy;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getParag() {
+        return Parag;
+    }
+
+    public void setParag(String parag) {
+        Parag = parag;
+    }
+
+    public String getAfterWhy() {
+        return AfterWhy;
+    }
+
+    public void setAfterWhy(String afterWhy) {
+        AfterWhy = afterWhy;
+    }
+
+    public AdviceParagraph(String title, String parag, String afterWhy) {
+        Title = title;
+        Parag = parag;
+        AfterWhy = afterWhy;
+    }
+    public AdviceParagraph() {    }
+
 }
