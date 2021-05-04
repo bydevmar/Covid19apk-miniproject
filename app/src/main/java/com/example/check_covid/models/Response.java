f4583e5aa0edc904013febf8f8a2cd74aec77f03
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-04 02:11:46 +0200

    SET Response CLASS

--- app/src/main/java/com/example/check_covid/models/Response.java
+++ app/src/main/java/com/example/check_covid/models/Response.java
@@ -1,4 +1,34 @@
 package com.example.check_covid.models;
 
+import java.util.List;
+
 public class Response {
+    private Global Global;
+    private List<Countries> Countries;
+    private String Date;
+
+    public Global getGlobal() {
+        return Global;
+    }
+
+    public void setGlobal(Global global) {
+        Global = global;
+    }
+
+    public List<Countries> getCountries() {
+        return Countries;
+    }
+
+    public void setCountries(List<Countries> countries) {
+        Countries = countries;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
 }
