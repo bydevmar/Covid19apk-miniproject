0ff1288d4d5ed660a652334fd65f4007cc697c63
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-04 02:05:56 +0200

    SET GLOBAL CLASS

--- app/src/main/java/com/example/check_covid/models/Global.java
+++ app/src/main/java/com/example/check_covid/models/Global.java
@@ -1,4 +1,59 @@
 package com.example.check_covid.models;
 
 public class Global {
+    private int NewConfirmed;
+    private int TotalConfirmed;
+    private int NewDeaths;
+    private int TotalDeaths;
+    private int NewRecovered;
+    private int TotalRecovered;
+
+    public int getNewConfirmed() {
+        return NewConfirmed;
+    }
+
+    public void setNewConfirmed(int newConfirmed) {
+        NewConfirmed = newConfirmed;
+    }
+
+    public int getTotalConfirmed() {
+        return TotalConfirmed;
+    }
+
+    public void setTotalConfirmed(int totalConfirmed) {
+        TotalConfirmed = totalConfirmed;
+    }
+
+    public int getNewDeaths() {
+        return NewDeaths;
+    }
+
+    public void setNewDeaths(int newDeaths) {
+        NewDeaths = newDeaths;
+    }
+
+    public int getTotalDeaths() {
+        return TotalDeaths;
+    }
+
+    public void setTotalDeaths(int totalDeaths) {
+        TotalDeaths = totalDeaths;
+    }
+
+    public int getNewRecovered() {
+        return NewRecovered;
+    }
+
+    public void setNewRecovered(int newRecovered) {
+        NewRecovered = newRecovered;
+    }
+
+    public int getTotalRecovered() {
+        return TotalRecovered;
+    }
+
+    public void setTotalRecovered(int totalRecovered) {
+        TotalRecovered = totalRecovered;
+    }
+
 }
