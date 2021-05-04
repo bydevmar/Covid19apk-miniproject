4ef22ba8a00451e5de484ec10ac0868bc5850031
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-04 15:53:19 +0200

    SET ListCountries CLASS

--- app/src/main/java/com/example/check_covid/models/ListCountries.java
+++ app/src/main/java/com/example/check_covid/models/ListCountries.java
@@ -1,4 +1,13 @@
 package com.example.check_covid.models;
 
-public class ListCountries {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListCountries extends ArrayList<Countries> {
+
+    public ListCountries(List<Countries> countries)  {
+        super();
+        addAll(countries);
+    }
 }
