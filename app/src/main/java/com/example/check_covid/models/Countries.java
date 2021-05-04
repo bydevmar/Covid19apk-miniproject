92a1bda018f9aceb49d7092cc12c03e90b9f4c1b
Abdelfattah Bouhlali <abdelfattah.bouhlali99@hotmail.com>
2021-05-04 02:08:25 +0200

    SET Countries CLASS

--- app/src/main/java/com/example/check_covid/models/Countries.java
+++ app/src/main/java/com/example/check_covid/models/Countries.java
@@ -1,4 +1,101 @@
 package com.example.check_covid.models;
 
 public class Countries {
+    protected String Country;
+    protected String CountryCode;
+    protected String Slug;
+    protected int NewConfirmed;
+    protected int TotalConfirmed;
+    protected int NewDeaths;
+    protected int TotalDeaths;
+    protected int NewRecovered;
+    protected int TotalRecovered;
+    protected String Date;
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public String getSlug() {
+        return Slug;
+    }
+
+    public void setSlug(String slug) {
+        Slug = slug;
+    }
+
+    public int getNewConfirmed() {
+        return NewConfirmed;
+    }
+
+    public void setNewConfirmed(int newConfirmed) {
+        NewConfirmed = newConfirmed;
+    }
+
+    public int getTotalConfirmed() {
+        return TotalConfirmed;
+    }
+
+    public void setTotalConfirmed(int totalConfirmed) {
+        TotalConfirmed = totalConfirmed;
+    }
+
+    public int getNewDeaths() {
+        return NewDeaths;
+    }
+
+    public void setNewDeaths(int newDeaths) {
+        NewDeaths = newDeaths;
+    }
+
+    public int getTotalDeaths() {
+        return TotalDeaths;
+    }
+
+    public void setTotalDeaths(int totalDeaths) {
+        TotalDeaths = totalDeaths;
+    }
+
+    public int getNewRecovered() {
+        return NewRecovered;
+    }
+
+    public void setNewRecovered(int newRecovered) {
+        NewRecovered = newRecovered;
+    }
+
+    public int getTotalRecovered() {
+        return TotalRecovered;
+    }
+
+    public void setTotalRecovered(int totalRecovered) {
+        TotalRecovered = totalRecovered;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Countries{" +
+                "Country='" + Country + '\'' +
+                '}';
+    }
 }
